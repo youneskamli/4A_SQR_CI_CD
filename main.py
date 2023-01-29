ca8f5570d9b46127fff7ef4a7b7436a15cc6d9cf
youneskamli <65299386+youneskamli@users.noreply.github.com>
2023-01-29 23:03:57 +0100

    Check syntax
    Ajout du check syntax dans le main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from Personne import Personne
 from operator import itemgetter
+import sys
 
 app = Flask(__name__)
  
@@ -85,6 +86,16 @@ def getSolde():
             solde_personne += str(element.solde)
             
     return solde_personne
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+            print("Passed argument not supported ! Supported argument : check_syntax")
+            exit(1)
+    app.run(debug=True)
 
 #curl -X POST -d "personne1=1&personne2=2&temps=10&somme=50" http://localhost:5000/transaction
 #
